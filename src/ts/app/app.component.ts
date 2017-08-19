52dfcc7b8aa32c163568729a27b421f1707b979f
Muhammad Assar <abu.assar@gmail.com>
2017-08-19 14:20:50 +0300

    part 4 reactive forms

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -1,11 +1,44 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, FormArray, FormBuilder } from "@angular/forms";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: require("./app.component.html"),
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public profileForm: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+        this.profileForm = this.fb.group({
+            firstNameInput: [""],
+            lastNameInput: [""],
+            addressGroups: this.fb.array([
+                this.fb.group({
+                    streetInput: [""],
+                    cityInput: [""],
+                    stateInput: [""],
+                    zipCodeInput: [""],
+                }),
+            ]),
+        });
+    }
+
+    public addAddressGroup() {
+        const fa = this.profileForm.controls["addressGroups"] as FormArray;
+
+        fa.push(new FormGroup({
+            streetInput: new FormControl(""),
+            cityInput: new FormControl(""),
+            stateInput: new FormControl(""),
+            zipCodeInput: new FormControl(""),
+        }));
+    }
+
+    /**
+     * save
+     */
+    public save() {
+        console.log(this.profileForm.value);
+    }
 }
